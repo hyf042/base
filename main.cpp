e769b18bc74fbfbe399eed0f9cdb1ec51a165cf4
hyf042 <hyf042@gmail.com0>
2012-12-18 16:00:11 +0800

    Add Codec.h&Net.h, and a custom codec RFA

--- main.cpp
+++ main.cpp
@@ -2,13 +2,26 @@
 #include <vector>
 #include "Base/Base.h"
 using namespace std;
+using namespace Base;
 
 using namespace Base::Math;
 
 int main(void)
 {
-	Vector3 p = Vector3(1,0,0);
-	p = p*Matrix::Rotate(pi*0.25f,Vector3(0,0,1));
-	cout << p.x << ' ' << p.y << ' ' << p.z << endl;
+	string a = "hyf hahahahaha yeah";
+	Raw data = a;
+	uint8 list[] = {89,95,22,0,103,227,146,36,15,208,92,45,13,3,196,11,26,159,28,10,35,239,4,12,60,192,191,111,48,215,39,99,30,176,156,0,6,212,247,120,32,120,92,21};
+	data = list;
+	//for(int i = 0; i < data.size(); i++)
+	//	cout << data[i] << endl;
+	//cout << "--------------------------------------------" << endl;
+	//data = Codec::RFA::instance().encode(data);
+	//for(int i = 0; i < data.size(); i++)
+	//	cout << uint32(data[i]) << ",";
+	//cout << endl;
+	//cout << "--------------------------------------------" << endl;
+	data = Codec::RFA::instance().decode(data);
+	for(int i = 0; i < data.size(); i++)
+		cout << data[i] << endl;
 	return 0;
 }
