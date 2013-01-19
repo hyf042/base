592f349a48204d07fe8a289c979ca78d56dc9ab4
hyf042 <hyf042@gmail.com0>
2013-01-19 16:47:37 +0800

    fix bugs about function invoking & string conv

--- main.cpp
+++ main.cpp
@@ -6,22 +6,14 @@ using namespace Base;
 
 using namespace Base::Math;
 
+void haha(int a, int b)
+{
+	cout << "haha: " << a+b << endl;
+}
+
 int main(void)
 {
-	string a = "hyf hahahahaha yeah";
-	Raw data = a;
-	uint8 list[] = {89,95,22,0,103,227,146,36,15,208,92,45,13,3,196,11,26,159,28,10,35,239,4,12,60,192,191,111,48,215,39,99,30,176,156,0,6,212,247,120,32,120,92,21};
-	data = list;
-	//for(int i = 0; i < data.size(); i++)
-	//	cout << data[i] << endl;
-	//cout << "--------------------------------------------" << endl;
-	//data = Codec::RFA::instance().encode(data);
-	//for(int i = 0; i < data.size(); i++)
-	//	cout << uint32(data[i]) << ",";
-	//cout << endl;
-	//cout << "--------------------------------------------" << endl;
-	data = Codec::RFA::instance().decode(data);
-	for(int i = 0; i < data.size(); i++)
-		cout << data[i] << endl;
+	Base::Function<void(int,int)> tmp = haha;
+	tmp(1,2);
 	return 0;
 }
